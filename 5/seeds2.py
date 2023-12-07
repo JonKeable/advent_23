f8e06fc29849a5e73ac087030998f73e4059380a
JonKeable <jekeable@live.co.uk>
2023-12-07 01:22:56 +0000

    not working on double overlap

--- 5/seeds2.py
+++ 5/seeds2.py
@@ -1,13 +1,65 @@
 import re
+from functools import total_ordering
 
-
-f = open("test.txt", 'r')
+f = open("input.txt", 'r')
 lineList = f.read().splitlines()
 
 #print(lineList)
 
 mappingsDict ={}
 mapOrder = []
+
+@total_ordering
+class RangeMap:
+    '''
+    represents the mapping of a source range to a destination range
+    '''
+    def __init__(self, start, range, offset) -> None:
+        self.sourceStart = start
+        self.sourceEnd = start+range
+        self.offset = offset
+        self.range = range
+        self.destStart = self.sourceStart + self.offset
+        self.destEnd = self.sourceEnd + self.offset
+        
+    def __lt__(self, other):
+        return self.sourceStart < other.sourceStart
+    def __repr__(self) -> str:
+        return('{' + str(self.sourceStart) + '-' + str(self.sourceEnd) + ' : ' + str(self.offset) +'}')
+    
+    def getDestRange(self, inputRange):
+        '''
+        given and input range, return a tuple containing 
+        -the range of destination values which are mapped from the given source input range
+        -the range of the given source input range which cannot be mapped
+        '''
+        inputStart = inputRange[0]
+        inputEnd = inputRange[1]
+        #no values in range
+        if inputStart > self.sourceEnd or  inputEnd <= self.sourceStart:
+            return (None, (inputStart, inputEnd))
+        #start within range
+        if inputStart >= self.sourceStart:
+            outputStart = inputStart+ self.offset
+            #fully inside
+            if inputEnd <= self.sourceEnd :
+                return ((outputStart, inputEnd+self.offset), None)
+            #overlap
+            else:
+                return((outputStart, self.sourceEnd),(self.sourceEnd, inputEnd))
+        #el start below range
+        #if end within range
+        elif inputEnd >= self.sourceStart :
+            outputStart = self.destStart
+            #underlap
+            if inputEnd <= self.sourceEnd :
+                return ((outputStart, inputEnd+self.offset), (inputStart, self.sourceStart))
+            #overlap and underlap
+            else:
+                return ([(inputStart, self.sourceStart-1), (outputStart, self.destEnd)], (self.sourceEnd, inputEnd))
+
+
+
 
 seeds = [int(seed) for seed in lineList[0].split(' ')[1:]]
 
@@ -18,7 +70,7 @@ for line in lineList[1:]:
         #print(mapping)
         source, dest = mapping[0], mapping[1]
         mappingName = source + '-' + dest
-        mappingsDict.update({mappingName : {}})
+        mappingsDict.update({mappingName : []})
         mapOrder.append(source)
     elif line != '':
         line = line.split()
@@ -26,28 +78,46 @@ for line in lineList[1:]:
         destStart = int(line[0])
         offset = destStart - sourceStart
         mapRange = int(line[2])
-        mappingsDict[mappingName].update({(sourceStart,sourceStart+mapRange):offset})
+        mappingsDict[mappingName].append(RangeMap(sourceStart,mapRange,offset))
 
 #not very encapsulated...
 mapOrder.append(dest)
 
-
-
-
+for mapList in mappingsDict.values():
+    mapList.sort()
         
-#print(mappingsDict)
+print(mappingsDict)
 #print(mappingsDict['seed-soil'])
 
-def getMapping(k, source, dest):
+
+def getMapping(kRange, source, dest):
+    '''
+    given a source range, get a list of all the mapped ranges
+    from the source property to the destination property
+    '''
     mapName = source + '-' + dest
-    map = mappingsDict[mapName]
-    for mapRange, offset in map.items():
-        if k in range(mapRange[0], mapRange[1]):
-            return k + offset
-    return k
+    #get the relevent mapping of source-dest
+    mapList = mappingsDict[mapName]
+    #this will store all the possible dest ranges for the source range
+    destRanges = []
+    remainRange = kRange
+    for rm in  mapList:
+        mapping = rm.getDestRange(remainRange)
+        mappedRange = mapping[0]
+        remainRange = mapping[1]
+        if mappedRange != None:
+            destRanges.append(mappedRange)
+        if remainRange == None:
+            break
+    if remainRange != None:
+        destRanges.append(remainRange)
+    #return the original range if no matching maps, 
+    # otherwise a list of all destination ranges plus unmapped ranges
+    #print(f'{kRange} {source} --> {destRanges} {dest}')
+    return destRanges
 
 
-locs = set()
+locs = list()
 #print(mapOrder)
 
 seedRanges = []
@@ -56,20 +126,44 @@ for seedStart in seedIt:
     seedRanges.append((seedStart, next(seedIt) + seedStart))
 
 print(seedRanges)
-
+print('|-|')
 for sr in seedRanges:
-    for seed in range(sr[0], sr[1]):
-        mapIt = iter(mapOrder)
-        start = next(mapIt)
-        res = seed
-        for id in mapIt:
-            end = id
-            res = getMapping(res, start, end)
-            start = end
-        locs.add(res)
+    #iterate over all the different properties, e.g. soil, water
+    orderIt = iter(mapOrder)
+    #start by getting the first property (seed)
+    start = next(orderIt)
+    #the range list for this property, starting with the seed ranges
+    rangeList = [sr]
+    #for every property in the mapping  order list
+    for id in orderIt:
+        #the end is the next property in the mapping
+        end = id
+        #the list of destination mapping ranges
+        nextList =[]
+        #for every source range in the list of ranges
+        for range in rangeList:
+            #get a list of all the mapped ranges for this property map
+            destRanges = getMapping(range, start, end)
+            nextList =destRanges
+        start = end
+        rangeList = nextList
+    locs.append(rangeList)
 
+
+
+print('---')
 print(locs)
-print(min(locs))
+
+minRange = locs[0][0][0]
+
+for rangeList in locs:
+    for range in rangeList:
+        print(range)
+        if range[0] < minRange:
+            minRange = range[0]
+
+print(minRange)
+
 
 
 
